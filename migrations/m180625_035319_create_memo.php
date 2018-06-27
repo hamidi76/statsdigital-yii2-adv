0e8eb5dd0281fdd2bd4bd1c8745456c0d37905cb
khairul faiz <mbn7938@gmail.com>
2018-06-27 09:53:52 +0800

    fixing migration error at create memo migration!!
    please dont change others migration code

--- migrations/m180625_035319_create_memo.php
+++ migrations/m180625_035319_create_memo.php
@@ -25,14 +25,6 @@ class m180625_035319_create_memo extends Migration
 
         $this->addForeignKey("memo_owner_fk", "memo", "memo_owner",
             "user", "id", "cascade", "cascade");
-
-        $columns = ['name','memo', 'memo_owner','status_id', 'created_at','updated_at'];
-        $this->batchInsert('{{%memo}}', $columns, [
-            ['Monday Memo','Training Memo Day 1',1, 1,date('Y-m-d '),date('Y-m-d '), gmdate('Y-m-d H:i:s'),gmdate('Y-m-d H:i:s')],
-            ['Tuesday Memo','Training Memo Day 2',1, 1,date('Y-m-d '),date('Y-m-d '),gmdate('Y-m-d H:i:s'),gmdate('Y-m-d H:i:s')],
-            ['Wednesday Memo','Training Memo Day 3',1, 1,date('Y-m-d '),date('Y-m-d '),gmdate('Y-m-d H:i:s'),gmdate('Y-m-d H:i:s')],
-            ['Thursday Memo','My Memo',1, 1,date('Y-m-d '),date('Y-m-d '),gmdate('Y-m-d H:i:s'),gmdate('Y-m-d H:i:s')],
-        ]);
     }
 
     /**
